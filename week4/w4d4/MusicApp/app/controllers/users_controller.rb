76cba9ae6dceae4c3f32f5b6d2b10e2fe3caa857
Eileen Ho <eileenho25@gmail.com>
2016-12-08 10:22:55 -0800

    add methods to users controller

---
+++ week4/w4d4/MusicApp/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    if current_user.id = params[:id]
+      @user = User.find(params[:id]).to_i
+      render :show
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+
+end
