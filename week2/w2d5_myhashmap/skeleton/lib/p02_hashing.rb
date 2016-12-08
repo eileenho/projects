92a9e46780ad70b25ad15c2521002f60d834a4c7
Eileen Ho <eileenho25@gmail.com>
2016-12-08 09:11:18 -0800

    week 2 incomplete project

--- week2/w2d5_myhashmap/skeleton/lib/p02_hashing.rb
+++ week2/w2d5_myhashmap/skeleton/lib/p02_hashing.rb
@@ -1,21 +1,68 @@
+# class Fixnum
+#   # Fixnum#hash already implemented for you
+# end
+
+# class Array
+#   def hash
+#     sum = 0
+#     self.each_with_index do |el, i|
+#       if el.is_a?(Fixnum)
+#         sum += el * i
+#       else
+#         sum += el.to_s.ord * i
+#       end
+#     end
+#     sum.hash
+#   end
+# end
+#
+# class String
+#   def hash
+#     sum = 0
+#     self.each_char.with_index do |char, i|
+#       sum += (char.ord * i)
+#     end
+#     sum.hash
+#   end
+# end
+#
+# class Hash
+#   # This returns 0 because rspec will break if it returns nil
+#   # Make sure to implement an actual Hash#hash method
+#   def hash
+#     array = self.keys.sort! + self.values.sort!
+#     sum = 0
+#     array.each_with_index do |el, i|
+#       if el.is_a?(Fixnum)
+#         sum += (el * i)
+#       else
+#         sum += el.to_s.ord * i
+#       end
+#     end
+#     sum.hash
+#   end
+# end
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    each_with_index.inject(0) do |intermediate_hash, (el, i)|
+      (el.hash + i.hash) ^ intermediate_hash
+    end
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    to_a.sort_by(&:hash).hash
   end
 end
--- week2/w2d5_myhashmap/skeleton/lib/p04_linked_list.rb
+++ week2/w2d5_myhashmap/skeleton/lib/p04_linked_list.rb
@@ -20,6 +20,10 @@ end
 
 class LinkedList
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +32,62 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    link = @tail.prev
+    until link.key == key
+      if link.key == key
+        return link.val
+      else
+        link = link.prev
+      end
+    end
   end
 
   def include?(key)
   end
 
   def append(key, val)
+    new_link = Link.new(key, val)
+
+    @tail.prev.next = new_link
+    new_link.next = @tail
+   @tail.prev = new_link
   end
 
   def update(key, val)
+    link_to_update = @tail.prev
+    unless link_to_update.key.nil?
+      until link_to_update.key == key
+        link_to_update = link_to_update.prev
+      end
+    end
+    link_to_update.val = val
   end
 
   def remove(key)
+    link = @tail.prev
+    unless link.key.nil?
+      until link.key == key
+        link = link.prev
+      end
+    end
+    link.prev.next = link.next
+    link.next.prev = link.prev
   end
 
   def each
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
