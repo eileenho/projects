c45fcf78ac96c9b7c3b6ebf8189bdf6ff1a9730c
Eileen Ho <eileenho25@gmail.com>
2016-12-13 17:03:36 -0800

    add sessions and routing phases

--- week5/w5d2_rails_lite/lib/controller_base.rb
+++ week5/w5d2_rails_lite/lib/controller_base.rb
@@ -8,33 +8,52 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "double render error" if already_built_response?
+    @already_built_response = true
+    @res['location'] = url
+    @res.status = 302
+    session.store_session(@res)
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render error" if already_built_response?
+    @already_built_response = true
+    @res['Content-type'] = content_type
+    @res.write(content)
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_name = File.join(dir_path, "..", "views", self.class.name.underscore, "#{template_name}.html.erb")
+    content = File.read(template_name)
+    template = ERB.new(content).result(binding)
+    render_content(template, "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    render(name)
   end
 end
-
